cd407b052650b6ba56702faca6978496ca2e7d93
yooeunji <yooeunji90@naver.com>
2016-08-18 19:24:11 +0900

    숙제 완료

--- JavaProgramming/src/ch04/homework/Homework1/Homework1.java
+++ JavaProgramming/src/ch04/homework/Homework1/Homework1.java
@@ -1,16 +1,16 @@
 package ch04.homework.Homework1;
 
-public class Homework1 {
+public class Homework1 { //클래스 선언
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { //메소드 선언
 		
-		for(int i=1; i<=100; i++){
-			int num1 =(int)(Math.random()*6)+1;
-			int num2 =(int)(Math.random()*6)+1;
+		for(int i=1; i<=100; i++){   //시행을 100번 반복
+			int num1 =(int)(Math.random()*6)+1;  //첫번째 주사위에서 나올수 있는 눈을 변수 num1에 저장
+			int num2 =(int)(Math.random()*6)+1;  //두번째 주사위에서 나올수 있는 눈을 변수 num2에 저장
 			
-			if(num1+num2==10){
-				System.out.println("(" + num1 +"," +num2 + ")");
+			if(num1+num2==10){ //둘의 합이 10이면
+				System.out.println("(" + num1 +"," +num2 + ")"); //출력
 			}
 		}
-}
+	}
 }
--- JavaProgramming/src/ch04/homework/Homework2/Homework2.java
+++ JavaProgramming/src/ch04/homework/Homework2/Homework2.java
@@ -6,8 +6,7 @@ public class Homework2 {
 		for(int i=1; i<=10; i++){
 			for(int j=1; j<=10; j++){
 				if((4*i)+(5*j)==60){
-					System.out.println("(" + i +","+ j +")");
-					
+					System.out.println("(" + i +","+ j +")");					
 				}
 			}
 		}
