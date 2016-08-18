9c833f2d9759e28178a2af2edb21b9e56e842581
yooeunji <yooeunji90@naver.com>
2016-08-18 19:33:09 +0900

    숙제끝

--- JavaProgramming/src/ch04/homework/Homework2/Homework2.java
+++ JavaProgramming/src/ch04/homework/Homework2/Homework2.java
@@ -1,12 +1,12 @@
-package ch04.homework.Homework2;
+package ch04.homework.Homework2; 
 
-public class Homework2 {
-	public static void main(String[] args) {
+public class Homework2 { //클래스 선언
+	public static void main(String[] args) { //메소드 선언
 		
-		for(int i=1; i<=10; i++){
-			for(int j=1; j<=10; j++){
-				if((4*i)+(5*j)==60){
-					System.out.println("(" + i +","+ j +")");					
+		for(int i=1; i<=10; i++){ //int형 변수 i를 선언하고 1부터 10까지 1씩증가하면서 반복
+			for(int j=1; j<=10; j++){ //int형 변수 j를 선언하고 1부터 10까지 1씩증가하면서 반복
+				if((4*i)+(5*j)==60){ //조건문을 만족하면
+					System.out.println("(" + i +","+ j +")");		 //출력			
 				}
 			}
 		}
