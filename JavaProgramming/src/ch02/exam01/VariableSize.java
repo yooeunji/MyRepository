0d6bc726db9ed566918cf3b0aa803e91682495f3
yooeunji <yooeunji90@naver.com>
2016-08-18 14:45:02 +0900

    VariableSize.java 추가

---
+++ JavaProgramming/src/ch02/exam01/VariableSize.java
@@ -0,0 +1,23 @@
+package ch02.exam01;
+
+public class VariableSize {
+
+	public static void main(String[] args) {
+		byte var1=10;
+		//byte var2=200;
+		
+		int var3=200;
+		byte var4=(byte)var3;
+		System.out.println(var4);
+		
+		/*
+		  int var5=200000;
+		 
+		char var6=(char)var5;
+		System.out.println(var6);
+		*/
+		// TODO Auto-generated method stub
+
+	}
+
+}
