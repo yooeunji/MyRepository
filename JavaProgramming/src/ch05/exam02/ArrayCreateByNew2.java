130dba929cd2fdf6bc2175cc16d88713b791136e
yooeunji <yooeunji90@naver.com>
2016-08-22 09:09:39 +0900

    ㅇㅇ

---
+++ JavaProgramming/src/ch05/exam02/ArrayCreateByNew2.java
@@ -0,0 +1,16 @@
+package ch05.exam02;
+
+public class ArrayCreateByNew2 {
+
+	public static void main(String[] args) {
+		int[] arr1=new int[5];
+		for(int i=0; i<arr1.length; i++){
+			System.out.println("arr1["+i+"]="+arr1[i]);
+		
+		}
+		
+		
+
+	}
+
+}
