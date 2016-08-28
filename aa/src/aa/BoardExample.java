364e5ad80a61bfa3bd3aaf3672c4280d5fe3dd51
yooeunji <yooeunji90@naver.com>
2016-08-29 07:13:56 +0900

    숙제

---
+++ aa/src/aa/BoardExample.java
@@ -0,0 +1,13 @@
+package aa;
+
+import java.util.Scanner;
+
+public class boardexample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in); //스캐너 타입의 변수 생성, 키보드로 입력받는값을 스캐너가 읽어들이겠다.
+
+	}
+
+}
