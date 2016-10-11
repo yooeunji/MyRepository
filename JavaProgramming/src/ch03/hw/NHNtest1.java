3b7bf8b32a93eaab907c4f239aa4b12b718039c9
yooeunji <yooeunji90@naver.com>
2016-10-11 17:22:46 +0900

    nhn

--- JavaProgramming/src/ch03/hw/NHNtest1.java
+++ JavaProgramming/src/ch03/hw/NHNtest1.java
@@ -1,9 +1,63 @@
 package ch03.hw;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
 public class NHNtest1 {
-
 	public static void main(String[] args) {
-		
-	}
+		   ArrayList<Integer> numberList = new ArrayList<Integer>();
+		   ArrayList<Sub> subList=new ArrayList<Sub>();
 
+	        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+	            String input = br.readLine();
+	            String[] numStrList = input.split(" ");
+	            for (String numStr : numStrList) {
+	                numberList.add(Integer.parseInt(numStr));
+	            }
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
+	        int j=1;
+	        for(int i=0; i<numberList.size(); i++){
+	        	j=1;
+	        	for(j=j+i; j<numberList.size(); j++){
+	        		Sub sub=new Sub(numberList.get(i), numberList.get(j));
+	        		subList.add(sub);
+	        	}
+	        }        
+	
+	int finalsub=subList.get(0).result;        
+	int finalsum=subList.get(0).sum;
+	Sub finalSub=new Sub(0, 0);
+	for(Sub s:subList){
+		if(s.result<=finalsub){
+			finalsub=s.result;
+			if(s.sum<finalsum){
+				System.out.println(s.num1+" "+ s.num2);
+			}
+			finalsum=s.sum;
+		}
+	}
+	
+	}
+	
+	public static class Sub{
+		int num1;
+		int num2;
+		int result;
+		int sum;
+		
+		Sub(int a, int b) {
+			num1=a;
+			num2=b;
+			sum=a+b;
+			if(a>b)
+				result=a-b;
+			else
+				result=b-a;
+		}
+	}
+	
 }
--- JavaProgramming/src/ch03/hw/NHNtest2.java
+++ JavaProgramming/src/ch03/hw/NHNtest2.java
@@ -1,10 +1,48 @@
 package ch03.hw;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class NHNtest2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+	
+	        String[] words = null;
+	        int num;
+	        int num1=0;
+	        int[] sol;
+	 
+	        
+	
+	        
+	       
+	        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+	            String line = br.readLine();
+	            words = line.split(" ");
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }   
+	        
+	        for(String word :words){
+	        	num=-1;
+	        	
+	        	for(int i=0; i<word.length(); i++){
+	        		char c=word.charAt(i);
+	        		
+	        		if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'){
+	        			if(i-num==1){
+	        				num1++;
+	        				break;
+	        			}
+	        		num=i; 		
+	        		} //모음확인
+	        		
+	        		else{ //자음 확인
+	        			
+	        		}
+	        	}
+	       }
+	        System.out.println(num1);
+	}  
 }
